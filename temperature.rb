b7a4a85435700def700986fcd931cdada65d117d
asjad-naeem <asjad.naeem@devsinc.com>
2023-07-18 13:02:52 +0500

    Input Format CHanged

--- temperature.rb
+++ temperature.rb
@@ -1,127 +1,95 @@
-require_relative "weather_man.rb"
-require "date"
-# This is a class for Temprature related stuff
-class Temperature < WeatherMan
-  def initialize
-    @max_temp = -9999
-    @min_temp = 9999
-    @max_day = ""
-    @min_day = ""
-    super
-  end
+# frozen_string_literal: true
 
-  def get_yealy_temp_report
-    puts "Enter Year:"
-    @year = gets.chomp
-    for totalfiles in 0..2
-      totalfiles == 0 ? limit = 1 : limit = 0
-      for totalmonths in 0..11
-        filename = "#{FILES[totalfiles]}_#{@year}_#{Date::MONTHNAMES[totalmonths + 1].slice(0, 3)}.txt"
-        data = []
-        data = File.readlines("./#{FILES[totalfiles]}/#{filename}") if File.exist?("./#{FILES[totalfiles]}/#{filename}")
-        for i in 0..data&.size
-          if (i > limit && data[i])
-            row_data = data[i].split ","
-            if row_data[1]&.size.to_i.positive? && row_data[1]&.to_i >= @max_temp.to_i
-              @max_temp = row_data[1]
-              @max_day = row_data[0]
-            end
-            if row_data[3]&.size.to_i.positive? && row_data[3]&.to_i <= @min_temp.to_i
-              @min_temp = row_data[3]
-              @min_day = row_data[0]
-            end
-          end
+require 'date'
+module Temperature
+  def get_yealy_temp_report(year, path)
+    max_temp = -9999
+    min_temp = 9999
+    max_day = ''
+    min_day = ''
+    limit = path.include?('lahore') ? 1 : 0
+    12.times do |totalmonths|
+      filename = "#{path}/#{path}_#{year}_#{Date::MONTHNAMES[totalmonths + 1].slice(0, 3)}.txt"
+      data = []
+      data = File.readlines(filename.to_s) if File.exist?(filename.to_s)
+      (0..data&.size).each do |i|
+        next if i <= limit || !data[i]
+
+        row_data = data[i].split ','
+        if row_data[1]&.size.to_i.positive? && row_data[1].to_i >= max_temp.to_i
+          max_temp = row_data[1].to_i
+          max_day = row_data[0]
+        end
+        if row_data[3]&.size.to_i.positive? && row_data[3].to_i <= min_temp.to_i
+          min_temp = row_data[3].to_i
+          min_day = row_data[0]
         end
       end
     end
-    display("No temperature related data exists for the given year") if @max_temp == -9999 && @min_temp == 9999
-    display("Max Temperature: #{@max_temp} and Day is: #{@max_day}\nMin Temperature: #{@min_temp} and Day is: #{@min_day}\n") if @max_temp != -9999 && @min_temp != 9999
+    return 'No temperature related data exists for the given year' if max_temp == -9999 && min_temp == 9999
+
+    output = "Max Temperature: #{max_temp} on: #{max_day}\nMin Temperature: #{min_temp} on: #{min_day}\n"
+    return output if max_temp != -9999 && min_temp != 9999
   end
 
-  def get_month_avg_report
-    puts "Enter Month(2003/1):"
-    input = gets.chomp
-    input = input.split "/"
-    @year = input[0]
-    @month = input[1].to_i - 1
-    if @month < 1 || @month > 12 || 
-      display("Incorrect Input months cannot be less than 0 or more than 12")
-    else
-      max_data_count = 0
-      max_sum = 0
-      min_data_count = 0
-      min_sum = 0
-      for totalfiles in 0..2
-        totalfiles == 0 ? limit = 1 : limit = 0
-        filename = "#{FILES[totalfiles]}_#{@year}_#{Date::MONTHNAMES[@month.to_i].slice(0, 3)}.txt"
-        data = []
-        data = File.readlines("./#{FILES[totalfiles]}/#{filename}") if File.exist?("./#{FILES[totalfiles]}/#{filename}")
-        for i in 0..data&.size
-          if (i > limit && data[i])
-            row_data = data[i].split ","
-            if row_data[1]&.size.to_i.positive?
-              max_sum += row_data[1].to_i
-              max_data_count += 1
-            end
-            if row_data[3]&.size.to_i.positive?
-              min_sum += row_data[3].to_i
-              min_data_count += 1
-            end
-          end
-        end
+  def get_month_avg_report(year, month, path)
+    max_data_count = 0
+    max_sum = 0
+    min_data_count = 0
+    min_sum = 0
+    limit = path.include?('lahore') ? 1 : 0
+    filename = "#{path}/#{path}_#{year}_#{Date::MONTHNAMES[month].slice(0, 3)}.txt"
+    data = []
+    data = File.readlines(filename.to_s) if File.exist?(filename.to_s)
+    (0..data&.size).each do |i|
+      next unless i > limit && data[i]
+
+      row_data = data[i].split ','
+      if row_data[1]&.size.to_i.positive?
+        max_sum += row_data[1].to_i
+        max_data_count += 1
       end
-      max_avg = max_sum / max_data_count if max_data_count > 0
-      min_avg = min_sum / min_data_count if min_data_count > 0
-      display("No temperature related data exists for the given year") if max_data_count == 0 && min_data_count == 0
-      display("Avg Max Temperature: #{max_avg} C\nAvg Min Temperature: #{min_avg} C\n") if max_data_count > 0 && min_data_count > 0
-    end
-  end
-
-  def get_complete_month_detail
-    puts "Enter Month(2003/1):"
-    input = gets.chomp
-    input = input.split "/"
-    @year = input[0]
-    @month = input[1].to_i - 1
-    if @month < 1 || @month > 12
-      display("Incorrect Input months cannot be less than 0 or more than 12")
-    else
-      max_data_hash = Hash.new
-      min_data_hash = Hash.new
-      for totalfiles in 0..2
-        totalfiles == 0 ? limit = 1 : limit = 0
-        filename = "#{FILES[totalfiles]}_#{@year}_#{Date::MONTHNAMES[@month.to_i].slice(0, 3)}.txt"
-        data = []
-        data = File.readlines("./#{FILES[totalfiles]}/#{filename}") if File.exist?("./#{FILES[totalfiles]}/#{filename}")
-        for i in 0..data&.size
-          if (i > limit && data[i])
-            row_data = data[i].split ","
-            if row_data[1]&.size.to_i.positive?
-              key = row_data[0].split "-"
-              if max_data_hash[key[2]] == nil
-                max_data_hash.store(key[2], row_data[1])
-              else
-                max_data_hash[key[2]] = row_data[1] if row_data[1].to_i > max_data_hash[key[2]].to_i
-              end
-            end
-            if row_data[3]&.size.to_i.positive?
-              key = row_data[0].split "-"
-              if min_data_hash[key[2]] == nil
-                min_data_hash.store(key[2], row_data[3])
-              else
-                min_data_hash[key[2]] = row_data[3] if row_data[3].to_i < min_data_hash[key[2]].to_i
-              end
-            end
-          end
-        end
+      if row_data[3]&.size.to_i.positive?
+        min_sum += row_data[3].to_i
+        min_data_count += 1
       end
     end
-    display_month_detail(max_data_hash, min_data_hash)
+    max_avg = max_sum / max_data_count if max_data_count.positive?
+    min_avg = min_sum / min_data_count if min_data_count.positive?
+    return 'No temperature related data exists for the given year' if max_data_count.zero? && min_data_count.zero?
+
+    output = "Avg Max Temperature: #{max_avg} C\nAvg Min Temperature: #{min_avg} C\n"
+    return output if max_data_count.positive? && min_data_count.positive?
   end
 
-  def select_temperature_action(user_choice)
-    get_yealy_temp_report if user_choice == 1
-    get_month_avg_report if user_choice == 2
-    get_complete_month_detail if user_choice == 3
+  def get_complete_month_detail(year, month, path)
+    max_data_hash = {}
+    min_data_hash = {}
+    limit = path.include?('lahore') ? 1 : 0
+    filename = "#{path}/#{path}_#{year}_#{Date::MONTHNAMES[month].slice(0, 3)}.txt"
+    data = []
+    data = File.readlines(filename) if File.exist?(filename)
+    (0..data&.size).each do |i|
+      next unless i > limit && data[i]
+
+      row_data = data[i].split ','
+      next unless row_data[1]&.size.to_i.positive?
+
+      key = row_data[0].split '-'
+      if max_data_hash[key[2]].nil?
+        max_data_hash.store(key[2], row_data[1])
+      elsif row_data[1].to_i > max_data_hash[key[2]].to_i
+        max_data_hash[key[2]] = row_data[1]
+      end
+      next unless row_data[3]&.size.to_i.positive?
+
+      key = row_data[0].split '-'
+      if min_data_hash[key[2]].nil?
+        min_data_hash.store(key[2], row_data[3])
+      elsif row_data[3].to_i < min_data_hash[key[2]].to_i
+        min_data_hash[key[2]] = row_data[3]
+      end
+    end
+    [max_data_hash, min_data_hash]
   end
 end
--- weather_man.rb
+++ weather_man.rb
@@ -1,61 +1,82 @@
-require "colorize"
+# frozen_string_literal: true
+
+$LOAD_PATH << '.'
+require 'colorize'
+require 'temperature'
+require 'humidity'
 # This is a class for Weather Data
 class WeatherMan
-  attr_accessor :user_input, :year, :month
-  FILES = %w[lahore_weather Murree_weather Dubai_weather]
-
-  def initialize
-    @user_input = -1
-    @year = ""
-    @month = ""
-  end
-
-  def menu
-    print <<MENU
-      1- For a given year display the highest temperature and day, lowest temperature and day, most humid day and humidity.
-      2- For a given month display the average highest temperature, average lowest temperature, average humidity.
-      3- For a given month draw two horizontal bar charts on the console for the highest and lowest temperature on each day.
-MENU
-    @user_input = gets
-    @user_input = @user_input.to_i
-    puts "Incorrect input! Please input from the given choices(1-3)" if @user_input > 3 || @user_input < 1
-  end
+  include Temperature
+  include Humidity
+  FILES = %w[lahore_weather Murree_weather Dubai_weather].freeze
 
   def display(output_data)
     puts output_data
   end
 
-  def display_month_detail(max_data_hash, min_data_hash)
-    max_data_hash&.each do |key, val|
-      index = 1
-      print key, " "
-      while index <= val.to_i
-        print "+".colorize(:red)
-        index += 1
-      end
-      print val, "C", "\n"
-      index = 1
-      print key, " "
-      while index <= min_data_hash[key].to_i
-        print "+".colorize(:blue)
-        index += 1
-      end
-      print min_data_hash[key], " C", "\n"
-    end
-    # Bonus
-    puts "Bonus Below:"
-    max_data_hash&.each do |key, val|
-      index = 1
-      print key, " "
-      while index <= val.to_i
-        if index <= min_data_hash[key].to_i
-          print "+".colorize(:blue)
-        else
-          print "+".colorize(:red)
+  def display_month_detail(hashes_arr)
+    max_data_hash = hashes_arr[0]
+    min_data_hash = hashes_arr[1]
+    if min_data_hash.size.positive? && max_data_hash.size.positive?
+      max_data_hash&.each do |key, val|
+        index = 1
+        print key, ' '
+        while index <= val.to_i
+          print '+'.colorize(:red)
+          index += 1
         end
-        index += 1
+        print val, 'C', "\n"
+        index = 1
+        print key, ' '
+        while index <= min_data_hash[key].to_i
+          print '+'.colorize(:blue)
+          index += 1
+        end
+        print min_data_hash[key], ' C', "\n"
       end
-      print val, " C", "\n"
+      # Bonus
+      puts 'Bonus Below:'
+      max_data_hash&.each do |key, val|
+        index = 1
+        print key, ' '
+        while index <= val.to_i
+          if index <= min_data_hash[key].to_i
+            print '+'.colorize(:blue)
+          else
+            print '+'.colorize(:red)
+          end
+          index += 1
+        end
+        print val, ' C', "\n"
+      end
+    else
+      puts 'No data for the given month is found'
     end
   end
+end
+if ARGV.length == 3
+  weather_man = WeatherMan.new
+  case ARGV[0]
+  when '-e'
+    weather_man.display(weather_man.get_yealy_temp_report(ARGV[1], ARGV[2]))
+    weather_man.display(weather_man.get_max_humidity(ARGV[1], ARGV[2]))
+  when '-a'
+    data = ARGV[1].split '/'
+    if data.length < 2
+      display('Incorrect Month Provided')
+    else
+      weather_man.display(weather_man.get_month_avg_report(data[0].to_i, data[1].to_i, ARGV[2]))
+      weather_man.display(weather_man.get_avg_humidity(data[0].to_i, data[1].to_i, ARGV[2]))
+    end
+  when '-c'
+    data = ARGV[1].split '/'
+    if data.length < 2
+      puts 'Incorrect input! Month not Provided'
+    else
+      data = ARGV[1].split '/'
+      weather_man.display_month_detail(weather_man.get_complete_month_detail(data[0].to_i, data[1].to_i, ARGV[2]))
+    end
+  end
+else
+  puts 'Incorrect input! Arguments are not complete!'
 end
